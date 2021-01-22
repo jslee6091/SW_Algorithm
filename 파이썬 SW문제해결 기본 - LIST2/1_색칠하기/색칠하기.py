a8e31f110243ad099b5e8f4c203c6086f36188b1
jslee6091 <jslee6091@naver.com>
2021-01-22 18:09:31 +0900

    sw expert academy

--- 파이썬 SW문제해결 기본 - LIST2/1_색칠하기/색칠하기.py
+++ 파이썬 SW문제해결 기본 - LIST2/1_색칠하기/색칠하기.py
@@ -15,10 +15,14 @@ for test_case in range(1, T + 1):
         r1, c1, r2, c2, color = map(int, input().split())
         for nemo1 in range(r1, r2 + 1):
             for nemo2 in range(c1, c2 + 1):
-                num_list[nemo1][nemo2] += color
+                if num_list[nemo1][nemo2] == 0:
+                    num_list[nemo1][nemo2] = color
+                elif num_list[nemo1][nemo2] != color:
+                    num_list[nemo1][nemo2] = 3
 
-    for check in num_list:
-        print(check)
+    answer = 0
+    for i in num_list:
+        answer += i.count(3)
 
-    print(f'{test_case} 번째 배열')
+    print(f'#{test_case} {answer}')
 
