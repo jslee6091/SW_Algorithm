31d466fef6cca1dc7cf6b07e5b39f2671454a8b4
jslee6091 <jslee6091@naver.com>
2021-01-31 22:44:33 +0900

    python algorithm

--- 파이썬 SW문제해결 기본 - Stack1/2_괄호검사/괄호검사.py
+++ 파이썬 SW문제해결 기본 - Stack1/2_괄호검사/괄호검사.py
@@ -1,7 +1,10 @@
 import sys
+
 sys.stdin = open("괄호검사_inputs.txt", 'r')
 
 T = int(input())
+
+ans_dict = {'}': '{', ')': '(', ']': '['}
 
 for test_case in range(1, T + 1):
     sentence = list(input())
@@ -9,24 +12,33 @@ for test_case in range(1, T + 1):
     answer = 1
 
     for idx in sentence:
-        if idx == '{' or idx == '(' or idx == '[':
+        # idx == '{' or idx == '(' or idx == '[' 을 values()로 표현
+        if idx in ans_dict.values():
             check.append(idx)
-        elif idx == '}' or idx == ']' or idx == ')':
+
+        # idx == '}' or idx == ']' or idx == ')' 을 keys()로 표현
+        elif idx in ans_dict.keys():
             if len(check) >= 1:
                 num = check.pop()
             else:
                 answer = 0
                 break
 
-            if idx == '}' and num != '{':
+            if ans_dict[idx] != num:
                 answer = 0
                 break
-            elif idx == ']' and num != '[':
-                answer = 0
-                break
-            elif idx == ')' and num != '(':
-                answer = 0
-                break
+
+            # 아래의 if elif 구문을 dictionary 를 이용하여 하나의 if 문으로 표현
+            # if idx == '}' and num != '{':
+            #     answer = 0
+            #     break
+            # elif idx == ']' and num != '[':
+            #     answer = 0
+            #     break
+            # elif idx == ')' and num != '(':
+            #     answer = 0
+            #     break
+
         else:
             pass
 
