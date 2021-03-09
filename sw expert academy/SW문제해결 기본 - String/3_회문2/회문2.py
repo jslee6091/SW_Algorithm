31fb87ec864bc1edc662fc34db4b046387262d42
jslee6091 <jslee6091@naver.com>
2021-03-09 18:36:04 +0900

    python algorithm

--- sw expert academy/SW문제해결 기본 - String/3_회문2/회문2.py
+++ sw expert academy/SW문제해결 기본 - String/3_회문2/회문2.py
@@ -3,29 +3,34 @@ sys.stdin = open("회문2_inputs.txt", 'r')
 
 
 def palindrome(array, n):
-    for j in range(100-n+1):
-        if array[j:j+n] == list(reversed(array[j:j+n])):
-            return 1
-    return 0
+    result = 0
+    for k in range(100-n+1):
+        if array[k:k+n] == list(reversed(array[k:k+n])):
+            result = 1
+            break
+    return result
 
 
 for test_case in range(1, 11):
     N = input()
     num_array = [list(map(str, list(input()))) for _ in range(100)]
-    number = 0
     answer = 0
 
-    for i in num_array:
-        while number <= 100:
+    for number in range(100, 0, -1):
+        number1 = 0
+        number2 = 0
+        for i in num_array:
             if palindrome(i, number):
-                answer = number
-            number += 1
+                number1 = 1
+                break
+        else:
+            for j in zip(*num_array):
+                if palindrome(list(j), number):
+                    number2 = 1
+                    break
 
-    number = 0
-    for j in zip(*num_array):
-        while number <= 100:
-            if palindrome(j, number):
-                answer = number
-            number += 1
+        if number1 or number2:
+            answer = number
+            break
 
     print(f'#{test_case} {answer}')
