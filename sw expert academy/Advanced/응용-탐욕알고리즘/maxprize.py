3182300b0cba130cfdda1987c3490d7b2bf820fb
jslee6091 <jslee6091@naver.com>
2021-06-18 02:05:03 +0900

    python algorithm

--- sw expert academy/Advanced/응용-탐욕알고리즘/maxprize.py
+++ sw expert academy/Advanced/응용-탐욕알고리즘/maxprize.py
@@ -7,20 +7,34 @@ for test_case in range(1, 1 + T):
     arr, N = map(str, input().split())
     arr_number = list(map(int, arr))
     N_int = int(N)
-    print('arr : ', arr_number, type(arr_number))
-    print('N : ', N_int, type(N_int))
-    print(max(arr_number[1:]), type(max(arr_number[1:])), arr_number.index(max(arr_number[1:])))
-    gg = arr_number.index(max(arr_number[1:]))
-    arr_number[0], arr_number[gg] = arr_number[gg], arr_number[0]
-    print(arr_number)
 
-    # index = 0
-    # while index != N_int:
-    #     card1, card2 = arr_number[0], max(arr_number[1:])
-    #     print('card1 : ', card1)
-    #     print('card2 : ', card2)
-    #     if card1 >= card2:
-    #         continue
-    #     else:
-    #         arr_number[0], arr_number[arr_number.index(card2)] = card2, card1
-    #         index += 1
+    index = 0
+    start = 0
+    while index != N_int:
+        card1 = arr_number[start]
+        card2 = arr_number[start + 1]
+        max_index = start + 1
+
+        for i in range(start + 2, len(arr_number)):
+            if arr_number[i] >= card2:
+                card2 = arr_number[i]
+                max_index = i
+
+        print('card1 : ', card1)
+        print('card2 : ', card2)
+        print('max_index : ', max_index)
+
+        if card1 >= card2:
+            start += 1
+            if start == len(arr_number) - 1:
+                break
+            else:
+                continue
+        else:
+            print('arr_number before : ', arr_number)
+            arr_number[start], arr_number[max_index] = card2, card1
+            index += 1
+            start += 1
+            print('arr_number after : ', arr_number)
+
+    print(f'#{test_case} {arr_number}')
