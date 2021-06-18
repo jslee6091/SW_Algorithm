c0d48d627230d72527764f6197052354b24628ba
jslee6091 <jslee6091@naver.com>
2021-06-18 17:51:32 +0900

    python algorithm

--- sw expert academy/Advanced/응용-탐욕알고리즘/maxprize.py
+++ sw expert academy/Advanced/응용-탐욕알고리즘/maxprize.py
@@ -3,6 +3,10 @@ sys.stdin = open('input.txt', 'r')
 
 T = int(input())
 
+# 탐욕 알고리즘 - 주어진 횟수만큼 숫자 위치를 바꾸어 가장 큰 수를 얻는 방법찾기
+# 내가 만든 코드 - 하나의 숫자를 선택 후 나머지 숫자 중 가장 큰 숫자를 선택하여 바꾸는 방법으로 구현
+# 32888의 경우 오류 발생
+'''
 for test_case in range(1, 1 + T):
     arr, N = map(str, input().split())
     arr_number = list(map(int, arr))
@@ -38,3 +42,48 @@ for test_case in range(1, 1 + T):
             print('arr_number after : ', arr_number)
 
     print(f'#{test_case} {arr_number}')
+'''
+
+
+# https://mungto.tistory.com/212 에서 참고한 코드
+# 깊이 우선 탐색과 가지치기 방법으로 최대수를 얻었다.
+# 바꿀수 있는 모든 경우를 고려하되 이미 탐색한 숫자는 제외하는 방식으로 가지치기 실행
+def dfs(count):
+    global answer
+    # 횟수를 다 사용한 경우
+    if not count:
+        # 숫자로 바꾸고 최대수인 경우 갱신
+        temp = int(''.join(values))
+        if answer < temp:
+            answer = temp
+        return
+    # 횟수가 남아있으므로 숫자 바꾸기 실행
+    for i in range(length):
+        # 경우의 수를 찾는거니까 i보다 큰위치부터
+        for j in range(i + 1, length):
+            # 두개의 위치를 바꾸고 나서
+            values[i], values[j] = values[j], values[i]
+            # 가지치기 위한 숫자 합치기
+            temp_key = ''.join(values)
+            # 어떤수가 몇회차에 나왔는지 체크, 없는 경우 1 리턴하고 경우의수에 넣어주기
+            if visited.get((temp_key, count - 1), 1):
+                # 이숫자는 몇회차에 사용했으니까 0으로 변환
+                visited[(temp_key, count - 1)] = 0
+                # dfs 실행
+                dfs(count - 1)
+            # 완료 후 원상복귀 - 다음 탐색을 위해
+            values[i], values[j] = values[j], values[i]
+
+
+for t in range(T):
+    answer = -1
+    value, change = input().split()
+    # 바꾸기 편하려고 리스트화시킴
+    values = list(value)
+    change = int(change)
+    # 계속 쓸꺼니까 캐스팅
+    length = len(values)
+    # 가지치기용 딕셔너리
+    visited = {}
+    dfs(change)
+    print('#{} {}'.format(t + 1, answer))
